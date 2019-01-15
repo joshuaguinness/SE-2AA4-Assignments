09a8192cd3eeb7da8469bd60d32fc43025a07778
guinnesj <guinnesj@mcmaster.ca>
2019-01-15 18:49:25 -0500

    Set up the function definitions and created a file of all the student data

--- A1/src/ReadAllocationData.py
+++ A1/src/ReadAllocationData.py
@@ -1,6 +1,15 @@
 ## @file ReadAllocationData.py
 #  @author 
 #  @brief 
-#  @date 
-# test
-print("Hello World")
+#  @date
+
+def readStdnts(s):
+    print("Starting to read students")
+
+def readFreeChoice(s):
+    print("Starting to read free choie")
+
+def readDeptCapacity(s):
+    print("Starting to read the department capacity")
+
+print("Started file ReadAllocationData")
