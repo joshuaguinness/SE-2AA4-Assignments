ec4ab78a72cb7d07193e57ba12d160c99be9414a
guinnesj <guinnesj@mcmaster.ca>
2019-01-16 18:05:37 -0500

    Set up the functions for step 2

--- A1/src/CalcModule.py
+++ A1/src/CalcModule.py
@@ -1,5 +1,17 @@
 ## @file CalcModule.py
-#  @author 
+#  @Joshua Guinness 
 #  @brief 
 #  @date 
 
+def sort(S):
+
+    
+    return student_dictionaries
+
+def average(L, g):
+
+    return average_gpa
+
+def allocate(S, F, C):
+
+    return dicionary
