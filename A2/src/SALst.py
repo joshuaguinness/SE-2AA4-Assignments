ec275d2a6a95b8cfe74a67eae6d9d112babeb686
guinnesj <guinnesj@mcmaster.ca>
2019-02-11 10:29:35 -0500

    Finished coding SALst

---
+++ A2/src/SALst.py
@@ -0,0 +1,24 @@
+# Created by Joshua Guinness
+
+import StdntAllocTypes
+import AALst
+import DCapALst
+
+class SALst:
+	def __init__(self):
+		self.s = []
+
+	def add(m, i):
+		
+
+	def remove():
+
+	def elm():
+
+	def info():
+
+	def sort():
+
+	def average():
+
+	def allocate
