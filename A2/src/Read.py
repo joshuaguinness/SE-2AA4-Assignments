613653713243fdb0b4c757718905e3ee197b960d
guinnesj <guinnesj@mcmaster.ca>
2019-02-11 12:08:20 -0500

    Started writing the read module

---
+++ A2/src/Read.py
@@ -0,0 +1,14 @@
+# Created by Joshua Guinness
+
+from StdntAllocTypes import *
+from DCapALst import *
+from SALst import *
+
+stdnt_data = "StdntData.txt"
+dept_capacity = "DeptCap.txt"
+
+def load_stdnt_data(s):
+
+
+
+def load_dcap_data(s):
