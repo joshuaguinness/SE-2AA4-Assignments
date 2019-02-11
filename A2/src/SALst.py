0c4cdb022a86802ba23242517f7a765041ef5c6f
guinnesj <guinnesj@mcmaster.ca>
2019-02-11 15:58:26 -0500

    Finished SALst module

--- A2/src/SALst.py
+++ A2/src/SALst.py
@@ -80,27 +80,68 @@ class SALst:
 	@staticmethod
 	def sort(f):
 		L = []
-		temp = SALst.s
-		while (len(temp) > 0):
-			for i in range(len(temp)):
-				if (get_gpa(temp[i][0], ))
-				
+		temp = SALst.s.copy()
 
+		for i in temp:
+			if (f(i[1]) == False):
+				temp.remove(i)
+
+		while (len(temp) > 0):
+			highest = -1
+			element_number = 0
+			for i in range(len(temp)):
+				if (temp[1].gpa > highest):
+					temp[1].gpa > highest
+					element_number = i
+
+			L.append(SALst.s[element_number][0])
+
+		return L
+				
 	## @brief Checks to see if the end of the sequence is reached
 	#  @param p1 The instance of the class
 	#  @return Boolean value about whether have reached end of sequence
 	@staticmethod
 	def average(f):
 
+		temp = SALst.s.copy()
+
+		for i in temp:
+			if (f(i[1]) == False):
+				temp.remove(i)
+
+		if (len(temp) == 0):
+			raise ValueError
+
+		total = 0
+		number = len(temp)
+
+		for i in temp:
+			total = total + i[1].gpa
+
+		return total/number
+
 	## @brief Checks to see if the end of the sequence is reached
 	#  @param p1 The instance of the class
 	#  @return Boolean value about whether have reached end of sequence
 	@staticmethod
-	def allocate
-
-	def __get_gpa__(m, s):
-		for i in SALst.s:
-			if (i[0] == m):
-				return i[1].gpa
-
+	def allocate:
 		
+		AALst.init()
+
+		F = SALst.sort(lambda t: t.freechoice and t.gpa >= 4.0)
+		for all m in F:
+			ch = SALst.info(m).choices
+			AALst.add_stdnt(ch.next(), m)
+
+		S = SALst.sort(lambda t: not t.freechoice and t.gpa >= 4.0)
+		for all m in S:
+			ch = SALst.info(m).choices
+			alloc = False
+			while (not alloc and not ch.end()):
+				d = ch.next()
+				if (AALst.num_alloc(d) < DCapALst.capacity(d)):
+					AALst.add_stdnt(d, m)
+					alloc = True
+			if (not alloc):
+				raise RuntimeError
