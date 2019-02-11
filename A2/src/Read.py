6050c4a5bb67c938a34606c39b517d156eb3df19
guinnesj <guinnesj@mcmaster.ca>
2019-02-11 12:14:54 -0500

    Working on read

--- A2/src/Read.py
+++ A2/src/Read.py
@@ -4,11 +4,26 @@ from StdntAllocTypes import *
 from DCapALst import *
 from SALst import *
 
-stdnt_data = "StdntData.txt"
-dept_capacity = "DeptCap.txt"
-
 def load_stdnt_data(s):
+
+	SALst.init()
+
+	f = open(s, 'r')
+
+	for line in f:
+		temp = line.split(',')
 
 
 
 def load_dcap_data(s):
+
+
+
+
+
+stdnt_data = "StdntData.txt"
+dept_capacity = "DeptCap.txt"
+
+load_stdnt_data(stdnt_data)
+load_dcap_data(dept_capacity)
+
