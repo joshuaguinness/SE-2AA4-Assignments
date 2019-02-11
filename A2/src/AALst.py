3aa22ec0d5abf1cac083f0e95a1a80a9bb4d3e61
guinnesj <guinnesj@mcmaster.ca>
2019-02-11 10:29:51 -0500

    Finished coding AALst

---
+++ A2/src/AALst.py
@@ -0,0 +1,26 @@
+# Created by Joshua Guinness
+
+import StdntAllocTypes
+
+class StdntAllocTypes:
+
+	def __init__(self):
+		self.s = []
+
+		for Dept in (DepT):
+			self.s.append((Dept, []))
+
+	def add_stdnt(dep, m):
+		for i in self.s
+			if (i[0] == dep):
+				i[1].append(m)
+
+	def lst_alloc(d):
+		for i in self.s:
+			if (i[0] == d):
+				return i[1]
+
+	def num_alloc(d):
+		for i in self.s:
+			if (i[0] == d):
+				return len(i[1])
