e912ecb250f0210bbdfa10be00cab064b6803aea
Joshua Guinness <joshua.guinness@gmail.com>
2019-03-19 10:11:13 -0400

    Got rid of the spaces to make it look cleaner

--- A3/implementation/include/GameBoard.h
+++ A3/implementation/include/GameBoard.h
@@ -1,4 +1,27 @@
 #ifndef A3_GAME_BOARD_H_
 #define A3_GAME_BOARD_H_
 
+#include "CardTypes.h"
+#include "CardStack.h"
+
+class BoardT {
+
+    private:
+
+    protected:
+
+    public:
+        
+        BoartT BoardT (std::vector<T> s);
+        bool is_valid_tab_mv(CategoryT a, unsigned int b, unsigned int c);
+        bool is_valid_waste_mv(CategoryT a, unsigned int b);
+        bool is_valid_deck_mv();
+        tab_mv(CategoryT a, unsigned int b, unsigned int c);
+        waste_mv(CategoryT a, unsigned int b);
+        deck_mv();
+        get_tab
+
+
+};
+
 #endif
