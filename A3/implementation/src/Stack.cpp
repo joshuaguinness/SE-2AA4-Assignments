63c4bff9d78d5d7f4b1f71e16246ef9c67e7f542
Joshua Guinness <joshua.guinness@gmail.com>
2019-03-19 10:12:22 -0400

    Realized things were wrong and changed them. Also left some notes on how to properly do things

--- A3/implementation/src/Stack.cpp
+++ A3/implementation/src/Stack.cpp
@@ -1,5 +1,15 @@
 // Implementation here
 
+#include "../include/Stack.h"
+
+using std::vector;
+
+// Public Methods
+template <class T>   // Need this in front of every method   
+StackT<T>::Stack(std::vector<T> s)
+{
+
+}
 
 // Keep this at bottom
-template class Stack<CardT>;
+
