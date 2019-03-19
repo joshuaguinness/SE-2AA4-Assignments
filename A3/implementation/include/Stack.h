d92eb14be157be91b70a91355c2d00a1be16500a
Joshua Guinness <joshua.guinness@gmail.com>
2019-03-18 23:11:59 -0400

    Finished Stack.h and CardStack.h

--- A3/implementation/include/Stack.h
+++ A3/implementation/include/Stack.h
@@ -1,4 +1,46 @@
 #ifndef A3_STACK_H_
 #define A3_STACK_H_
 
+#include <vector>
+
+template <class T>
+class StackT {
+    private:
+        std::vector<T> s;
+
+    protected:
+
+    public:
+        Stack<T> Stack (std::vector<T> s)
+        {
+
+        }
+
+        Stack<T> push(T a)
+        {
+
+        }
+
+        Stack<T> pop ()
+        {
+
+        }
+
+        T top()
+        {
+
+        }
+
+        unsigned int size()
+        {
+
+        }
+
+        std::vector<T> toSeq()
+        {
+
+        }
+            
+};
+
 #endif
