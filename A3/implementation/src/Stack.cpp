ece5f4ff5ece6aaec1421b578a3ad131a5e1762e
Joshua Guinness <joshua.guinness@gmail.com>
2019-03-19 22:29:18 -0400

    Finished implementing Stack.cpp

--- A3/implementation/src/Stack.cpp
+++ A3/implementation/src/Stack.cpp
@@ -1,57 +1,60 @@
 // Implementation here
 
 #include "../include/Stack.h"
+#include <exception>
 
 using std::vector;
-
-/*
-private:
-        std::vector<T> s;
-
-    public:
-        Stack (std::vector<T> s);
-        Stack<T> push(T a);
-        Stack<T> pop ();
-        T top();
-        unsigned int size();
-        std::vector<T> toSeq();
-*/
 
 // Public Methods
 template <class T>   // Need this in front of every method   
 StackT<T>::Stack(std::vector<T> s)
 {
-
+    this->s = s;
 }
 
 template <class T>
 Stack<T> Stack<T>::push(T a)
 {
-
+    StackT<T> stack = StackT<T>(this->s.push_back(a));
+    return stack;
 }
 
 template <class T>
 Stack<T> Stack<T>::pop()
 {
-
+    if (this->s.size() == 0)
+    {
+        throw out_of_range();
+    } else
+    {
+        StackT<T> stack = StackT<T>(this->s.pop_back());
+        return stack;
+    }
 }
 
 template <class T>
 T Stack<T>::top()
 {
-
+    // https://www.programiz.com/cpp-programming/if-else
+    if (this->s.size() == 0)
+    {
+        throw out_of_range();
+    } else
+    {
+        return s.back();
+    }
 }
 
 template <class T>
 unsigned int Stack<T>::size()
 {
-
+    return this->s.size();
 }
 
 template <class T>
 std::vector<T> Stack<T>::toSeq()
 {
-    
+    return this->s;
 }
 
 // Private Methods
