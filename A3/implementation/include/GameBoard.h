6f5219c3a98ba09e5e0bc5bcae11f0d3b59c3f40
Joshua Guinness <joshua.guinness@gmail.com>
2019-03-20 17:27:44 -0400

    Continue editing gameboard as realize previous mistakes

--- A3/implementation/include/GameBoard.h
+++ A3/implementation/include/GameBoard.h
@@ -4,26 +4,27 @@
 #include "CardTypes.h"
 #include "CardStack.h"
 
+typedef std::vector<CardStackT> SeqCrdStckT;
+
 class BoardT {
 
     private:
-
         // Private Variables
         // https://www.cpp.edu/~elab/ECE114/Array.html
-        CardStackT t [8];
-        CardStack f [10];
+        std::vector<CardStackT> t;
+        std::vector<CardStackT> f;
         CardStackT d;
-        CardStack w;
+        CardStackT w;
 
         // Private Local Methods
         //https://www.tutorialspoint.com/cplusplus/cpp_passing_arrays_to_functions.htm
-        bool two_decks(CardStackT t[8], CardStack f[10], CardStackT d, CardStackT w);
+        bool two_decks(CardStackT t[8], CardStackT f[10], CardStackT d, CardStackT w);
         //unsigned int cnt_cards_seq(CardStackT S[], LAMBDA func);
         //unsigned int cnt_cards_stack(CardStackT s, LAMBDA func);
         //unsigned int cnt_cards(CardStackT t[8], CardStack f[10], CardStackT d, CardStackT w, LAMBDA func);
         // https://www.tutorialspoint.com/cplusplus/cpp_return_arrays_from_functions.htm
         CardStackT * init_seq(unsigned int n);
-        CardStackT * tab_deck(CardT a);
+        std::vector<CardStackT> tab_deck(std::vector<CardT> a);
         bool is_valid_pos(CategoryT a, unsigned int b);
         bool valid_tab_tab(unsigned int a, unsigned int b);
         bool valid_tab_foundation(unsigned int a, unsigned int b);
@@ -34,14 +35,13 @@ class BoardT {
 
     // Public Methods
     public:
-        
-        BoartT BoardT (std::vector<T> s);
+        BoardT(std::vector<CardT> s);
         bool is_valid_tab_mv(CategoryT a, unsigned int b, unsigned int c);
         bool is_valid_waste_mv(CategoryT a, unsigned int b);
         bool is_valid_deck_mv();
-        tab_mv(CategoryT a, unsigned int b, unsigned int c);
-        waste_mv(CategoryT a, unsigned int b);
-        deck_mv();
+        void tab_mv(CategoryT a, unsigned int b, unsigned int c);
+        void waste_mv(CategoryT a, unsigned int b);
+        void deck_mv();
         CardStackT get_tab(unsigned int a);
         CardStackT get_foundation(unsigned int a);
         CardStackT get_deck();
