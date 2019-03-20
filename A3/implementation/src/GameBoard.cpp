5c0404e39d451e734d022c6e9b98f92ef4c6cfa1
Joshua Guinness <joshua.guinness@gmail.com>
2019-03-20 17:28:57 -0400

    Continue to work on, finished the constructor, except the exceptoin ;) lol

--- A3/implementation/src/GameBoard.cpp
+++ A3/implementation/src/GameBoard.cpp
@@ -6,7 +6,73 @@
 #include <iostream>
 #include <algorithm>
 
+// Public Methods
+
 BoardT::BoardT(std::vector<CardT> s)
 {
-    std::cout << 1 << 2 << 3;
+    std::vector<CardT> front_of_deck(s.begin(), s.begin() + 40);
+    std::vector<CardT> end_of_deck(s.begin()+41, s.end());
+
+    CardStackT initial_deck = CardStackT(end_of_deck);
+    CardStackT initial_waste = CardStackT();
+
+    this->t = tab_deck(front_of_deck);
+    this->f = init_seq(8);
+    this->f = temp;
+    this->w = initial_waste;
+}
+
+bool BoardT::is_valid_tab_mv(CategoryT a, unsigned int b, unsigned int c)
+{
+
+}
+bool BoardT::is_valid_waste_mv(CategoryT a, unsigned int b)
+{
+
+}
+bool BoardT::is_valid_deck_mv()
+{
+
+}
+void BoardT::tab_mv(CategoryT a, unsigned int b, unsigned int c)
+{
+
+}
+void BoardT::waste_mv(CategoryT a, unsigned int b)
+{
+
+}
+void BoardT::deck_mv()
+{
+
+}
+CardStackT BoardT::get_tab(unsigned int a)
+{
+
+}
+CardStackT BoardT::get_foundation(unsigned int a)
+{
+
+}
+CardStackT BoardT::get_deck()
+{
+
+}
+CardStackT BoardT::get_waste()
+{
+
+}
+bool BoardT::valid_mv_exists()
+{
+
+}
+bool BoardT::is_win_state()
+{
+
+}
+
+// Private Methods
+std::vector<CardStackT> tab_deck(std::vector<CardT> a)
+{
+
 }
